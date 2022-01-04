15fcd205ffc1ec2ee0499531438e8a1aac2ffd11
Is olis <you@example.com>
2022-01-04 15:39:48 -0600

    el primero

---
+++ Zinnia/compara.js
@@ -0,0 +1,73 @@
+class BeforeAfter {
+    constructor(enteryObject) {
+
+        const beforeAfterContainer = document.querySelector(enteryObject.id);
+        const before = beforeAfterContainer.querySelector('.bal-before');
+        const beforeText = beforeAfterContainer.querySelector('.bal-beforePosition');
+        const afterText = beforeAfterContainer.querySelector('.bal-afterPosition');
+        const handle = beforeAfterContainer.querySelector('.bal-handle');
+        var widthChange = 0;
+
+        beforeAfterContainer.querySelector('.bal-before-inset').setAttribute("style", "width: " + beforeAfterContainer.offsetWidth + "px;")
+        window.onresize = function () {
+            beforeAfterContainer.querySelector('.bal-before-inset').setAttribute("style", "width: " + beforeAfterContainer.offsetWidth + "px;")
+        }
+        before.setAttribute('style', "width: 50%;");
+        handle.setAttribute('style', "left: 50%;");
+
+
+        function slideMove(e) {
+            var pos;
+            /*if the slider is no longer clicked, exit this function:*/
+            if (clicked == 0) return false;
+            /*get the cursor's x position:*/
+            pos = getCursorPos(e)
+            /*prevent the slider from being positioned outside the image:*/
+            if (pos < 0) pos = 0;
+            if (pos > w) pos = w;
+            /*execute a function that will resize the overlay image according to the cursor:*/
+            slide(pos);
+        }
+
+        function slide(x) {
+            /*resize the image:*/
+            before.style.width = x + "px";
+            /*position the slider:*/
+            handle.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
+        }
+        //touch screen event listener
+        beforeAfterContainer.addEventListener("touchstart", (e) => {
+
+            beforeAfterContainer.addEventListener("touchmove", (e2) => {
+                let containerWidth = beforeAfterContainer.offsetWidth;
+                let currentPoint = e2.changedTouches[0].clientX;
+
+                let startOfDiv = beforeAfterContainer.offsetLeft;
+
+                let modifiedCurrentPoint = currentPoint - startOfDiv;
+
+                if (modifiedCurrentPoint > 10 && modifiedCurrentPoint < beforeAfterContainer.offsetWidth - 10) {
+                    let newWidth = modifiedCurrentPoint * 100 / containerWidth;
+
+                    before.setAttribute('style', "width:" + newWidth + "%;");
+                    afterText.setAttribute('style', "z-index: 1;");
+                    handle.setAttribute('style', "left:" + newWidth + "%;",slideMove);
+                }
+            });
+        });
+
+        //mouse move event listener
+        beforeAfterContainer.addEventListener('mousemove', (e) => {
+            let containerWidth = beforeAfterContainer.offsetWidth;
+            widthChange = e.offsetX;
+            let newWidth = widthChange * 100 / containerWidth;
+
+            if (e.offsetX > 10 && e.offsetX < beforeAfterContainer.offsetWidth - 10) {
+                before.setAttribute('style', "width:" + newWidth + "%;");
+                afterText.setAttribute('style', "z-index:" + "1;");
+                handle.setAttribute('style', "left:" + newWidth + "%;",slideMove);
+            }
+        })
+
+    }
+}
